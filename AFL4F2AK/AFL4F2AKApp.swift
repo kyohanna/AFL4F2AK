27ff4141b53f71563905961985a1e69f1fd6dcc7
Kezia <kyohanna@student.ciputra.ac.id>
2022-05-24 08:54:35 +0700

    Initial Commit

---
+++ AFL4F2AK/AFL4F2AKApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4F2AKApp.swift
+//  AFL4F2AK
+//
+//  Created by SIFT-DBT Telkom Air 10 on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4F2AKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL4F2AK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AFL4F2AK
+//
+//  Created by SIFT-DBT Telkom Air 10 on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
